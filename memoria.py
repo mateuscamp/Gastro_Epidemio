26f3fb7c4d93e447e5e568cadd9f1ee804ed738f
Mateus Campelo <mateuscampelob@gmail.com>
2026-04-20 01:18:22 -0300

    build: minhas modificações proprias

--- memoria.py
+++ memoria.py
@@ -3,8 +3,8 @@ import pele
 
 def buscarquivo(nome_arquivo):
     try:
-        a = open(nome_arquivo, "rt")
-        a.close()
+        with open(nome_arquivo, "rt") as _: # não vou usar Variavel
+            pass
     except FileNotFoundError:
         return False
     else:
@@ -13,43 +13,65 @@ def buscarquivo(nome_arquivo):
 
 def criarquivo(nome_arquivo):
     try:
-        a = open(nome_arquivo, "wt+")
+        with open(nome_arquivo, "wt+") as _: # não vou usar Variavel
+            pass
     except Exception:
         print(
             "\033[31mErro na criação do arquivo\033[m"
         )  # Se houver erro inexperado ou falta de memoria
     else:
         print(f"\033[32m{nome_arquivo} criado com sucesso!\033[m")
-        a.close()
 
 
 def leiarquivo(nome_arquivo):
     try:
-        a = open(nome_arquivo, "rt")
+        with open(nome_arquivo, "rt") as a:
+            for linha in a:
+                dado = linha.replace("\n", "").split(';')
+                print(f"{dado[0]:<30}{dado[1]:>3} anos")
+
     except Exception:
-        pele.msg_erro("Erro ao ler PESSOAS CADASTRADAS")
-    else:
-        for linha in a:
-            dado = linha.replace("\n", "")
-            dado = dado.split(";")
-            print(f"{dado[0]:<30}{dado[1]:>3} anos")
-        a.close()
+        pele.msg_erro("Erro ao ler o arquivo")
 
 
 def cadastrarquivo(nome_arquivo, nome="desconhecido", idade="0", polipo="0"):
     try:
-        a = open(nome_arquivo, "at")
+        with open(nome_arquivo, "at") as a:
+            try:
+                a.write(f"{nome};{idade};{polipo}\n")
+
+            except Exception:
+                pele.msg_erro("Houve um erro na escrita dos dados")
+
     except Exception:
         pele.msg_erro("Houve um erro na abertura do banco de dados")
-    else:
-        try:
-            a.write(f"{nome};{idade};{polipo}\n")
-        except Exception:
-            pele.msg_erro("Houve um erro na escrita dos dados")
-        else:
-            a.close()
+
+
+def ordenarquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'rt') as a:
+            linhas = a.readlines() # Variavel linhas guarda a lista
+            linhas.sort()
+        with open(nome_arquivo, 'wt') as a:
+            a.writelines(linhas)
+
+    except Exception as e:
+        pele.msg_erro(f'Erro ao organizar o arquivo: {e}')
+
+
+def buscarpaciente(nome_arquivo, nome_procurado):
+    try:
+        with open(nome_arquivo, 'rt') as a:
+            for linha in a:
+                dado = linha.strip().split(';')
+                if dado[0].upper() == nome_procurado.upper():
+                    return dado
+    except Exception:
+        pele.msg_erro('Erro ao buscar. Por favor, tente novamente')
+    return pele.msg_aviso('Não encontrado') # se o loop acabar sem o dado
 
 
 def editarquivo(nome_arquivo, nome, idade, polipo):
     try:
-        a = open(nome_arquivo, 'wt+')
+        with open(nome_arquivo, 'wt') as a:
+        
--- programa.py
+++ programa.py
@@ -18,13 +18,22 @@ while True:
 
     elif opçao == 2:
         pele.cabeçalho("NOVO CADASTRO")
-        nome = input("Nome: ").strip().capitalize()
+        nome = input("Nome: ").strip().upper()
         idade = leiaint("Idade: ")
-        polipo = leiaint('Polipo: ')
+        polipo = leiaint("Polipo: ")
         memoria.cadastrarquivo(arquivo_bd, nome, idade, polipo)
-        pele.msg_sucesso('Cadastro concuído com sucesso!')
+        memoria.ordenarquivo(arquivo_bd)
+        pele.msg_sucesso("Cadastro concuído com sucesso!")
 
     elif opçao == 3:
+        pele.cabeçalho("EDITAR CADASTRO")
+        nome = input("Nome completo do paciente que deseja editar:")
+        memoria.buscarpaciente(arquivo_bd)
+
+    elif opçao == 4:
+        pele.cabeçalho("EXCLUIR CADASTRO")
+
+    elif opçao == 5:
         pele.msg_sucesso("Obrigado por usar o programa. Até logo!")
         break
 
